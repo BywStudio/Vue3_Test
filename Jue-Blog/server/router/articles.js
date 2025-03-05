84e094a34ab79c7de32d792136f92a37e1d5d183
BywStudio <19941829077@163.com>
2025-03-05 14:00:36 +0800

    完成文章详情接口的开发与测试

--- Jue-Blog/server/router/articles.js
+++ Jue-Blog/server/router/articles.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var ArtsModel = require('../model/articles')
+var UsersModel = require('../model/users')
 
 router.all('/', (req, res) => {
   res.send('文章管理 API')
@@ -119,6 +120,100 @@ router.get('/list', async (req, res, next) => {
     ])
     res.send(result)
   }catch(err){
+    next(err)
+  }
+})
+/// 文章详情
+router.get('/detail/:id', async (req, res, next) => {
+  let { id } = req.params
+  let user_id = req.auth ? req.auth._id : null
+  try {
+    let result = await ArtsModel.aggregate([
+      {
+        $match: {
+          _id: ObjectId(id),
+        },
+      },
+      {
+        $lookup: {
+          from: 'praises',
+          localField: '_id',
+          foreignField: 'target_id',
+          as: 'praises',
+        },
+      },
+      {
+        $lookup: {
+          from: 'comments',
+          localField: '_id',
+          foreignField: 'source_id',
+          as: 'comments',
+        },
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'created_by',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      {
+        $addFields: {
+          praises: {
+            $filter: {
+              input: '$praises',
+              as: 'arrs',
+              cond: { $eq: ['$$arrs.type', 1] },
+            },
+          },
+          comments: {
+            $size: '$comments',
+          },
+          stars: {
+            $filter: {
+              input: '$praises',
+              as: 'arrs',
+              cond: { $eq: ['$$arrs.type', 2] },
+            },
+          },
+          user: {
+            $first: '$user',
+          },
+        },
+      },
+      {
+        $addFields: {
+          is_praise: {
+            $in: [ObjectId(user_id), '$praises.created_by'],
+          },
+          praises: {
+            $size: '$praises',
+          },
+          is_start: {
+            $in: [ObjectId(user_id), '$stars.created_by'],
+          },
+          stars: {
+            $size: '$stars',
+          },
+        },
+      },
+      {
+        $unset: ['user.password', 'user.__v'],
+      },
+    ])
+    if (result.length > 0) {
+      await ArtsModel.findByIdAndUpdate(id, {
+        $inc: { page_view: 1 },
+      })
+      await UsersModel.findByIdAndUpdate(result[0].created_by, {
+        $inc: { read_num: 1 },
+      })
+      res.send(result[0])
+    } else {
+      res.status(400).send({ message: '文章不存在' })
+    }
+  } catch (err) {
     next(err)
   }
 })
